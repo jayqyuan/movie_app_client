c337fb02812502d4ceef0459b62bd3ecc2ec5eac
jayqyuan <118870651+jayqyuan@users.noreply.github.com>
2024-12-02 12:48:10 -0500

    fixed carousel display

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -9,10 +9,18 @@ const Hero = ({movies}) => {
             {
                 movies.map((movie)=>{
                     return(
-                        <Paper>
+                        <Paper key={movie.id}>
                             <div className='movie-card-container'>
-                                <div>
-                                    
+                                <div className='movie-card'>
+                                    <div className='movie-detail'>
+                                        <div className='movie-poster'>
+                                            <img src={movie.poster} alt=''/>
+                                        </div>
+                                        <div className='movie-title'>
+                                            <h4>{movie.title}</h4>
+                                        </div>
+                                    </div>
+
                                 </div>
                             </div>
                         </Paper>
