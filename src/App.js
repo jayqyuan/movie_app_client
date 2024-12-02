39747022918dedb385c087cfb80c9fb8aabad324
jayqyuan <118870651+jayqyuan@users.noreply.github.com>
2024-12-02 10:35:15 -0500

    set up layout componenet and establish routers

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import api from './api/axiosConfig';
 import { useState, useEffect } from 'react';
+import Layout from './components/Layout';
+import { Routes, Route } from 'react-router-dom';
+
 
 function App() {
 
@@ -11,8 +14,6 @@ function App() {
     try 
     {
       const response = await api.get('/api/v1/movies');
-
-      console.log(response.data);
 
 			setMovies(response.data);
 
@@ -27,6 +28,11 @@ function App() {
   }, [])
   return (
     <div className="App">
+      <Routes>
+        <Route path='/' element={Layout}>
+
+        </Route>
+      </Routes>
     
     </div>
   );
