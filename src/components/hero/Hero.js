6d6789520a8fc186ccb81b7d1f96fa9be55dbc44
jayqyuan <118870651+jayqyuan@users.noreply.github.com>
2024-12-02 13:36:27 -0500

    updated homepage styling for the hero component

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,3 +1,4 @@
+import './Hero.css'
 import React from 'react'
 import Carousel from 'react-material-ui-carousel';
 import { Paper } from '@mui/material'
@@ -11,7 +12,7 @@ const Hero = ({movies}) => {
                     return(
                         <Paper key={movie.id}>
                             <div className='movie-card-container'>
-                                <div className='movie-card'>
+                                <div className='movie-card' style={{"--img":`url(${movie.backdrops[0]})`}}>
                                     <div className='movie-detail'>
                                         <div className='movie-poster'>
                                             <img src={movie.poster} alt=''/>
